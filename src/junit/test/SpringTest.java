ed99629d0f89691cb7479e6400db63bccddcf5fb
dravenxiaokai <jspankai@vip.qq.com>
2018-03-30 18:42:44 +0800

    新建java工程，配置spring环境，搭建测试环境

---
+++ src/junit/test/SpringTest.java
@@ -0,0 +1,14 @@
+package junit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpringTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
